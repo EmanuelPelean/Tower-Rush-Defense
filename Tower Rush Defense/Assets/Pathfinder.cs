0887824e42702569c3df709d936e25a694de1831
Emanuel Pelean <32735104+EmanuelPelean@users.noreply.github.com>
2019-03-08 23:44:25 -0500

    add pathfinder script which intially stores all waypoints

---
+++ Tower Rush Defense/Assets/Pathfinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+	void Start () {
+        loadBlocks();
+	}
+
+    private void loadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            bool isOverlapping = grid.ContainsKey(waypoint.getGridPos());
+            if(isOverlapping)
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            } else
+            {
+                grid.Add(waypoint.getGridPos(), waypoint);
+            }
+            
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+}
